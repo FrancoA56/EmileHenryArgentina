979ce26f82e00ebaf8e43becf480ff39feda9a6b
FrancoA56 <franco.adamoli@gmail.com>
2025-11-11 17:45:07 -0300

    banner wholesale 3 parts

--- components/dondeComprarMayorista.js
+++ components/dondeComprarMayorista.js
@@ -3,20 +3,54 @@ import Image from "next/image";
 import styles from "./dondeComprarMayorista.module.css";
 
 export default function DondeComprarMayorista() {
-  const denisRos = "/img/wholesale-banner.png";
+  const bannerTop = "/img/wholesaleBannerTop.png";
+  const bannerWsp = "/img/wholesaleBannerWsp.png";
+  const bannerMail = "/img/wholesaleBannerMail.png";
 
   return (
     <section className={styles.donde_comprar_main} id="donde-comprar">
       <h2 className={styles.donde_comprar_title}>NUESTRO CANAL MAYORISTA</h2>
 
-      <div className={styles.imgContainer}>
+      <div className={styles.bannerContainer}>
+        {/* Parte superior */}
         <Image
-          src={denisRos}
-          alt="denisRos"
-          width={1200}   // poné el ancho real de tu imagen
-          height={310}   // y el alto real (se ajustará automáticamente)
-          className={styles.img}
+          src={bannerTop}
+          alt="Banner mayorista superior"
+          width={2400}
+          height={304}
+          className={styles.bannerTop}
         />
+
+        {/* Parte inferior con dos imágenes lado a lado */}
+        <div className={styles.bannerBottom}>
+          <a
+            href="https://wa.me/5491123053139"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              src={bannerWsp}
+              alt="Contacto por WhatsApp"
+              width={1200}
+              height={316}
+              className={styles.bannerBottomImg}
+            />
+          </a>
+
+          <a
+            href="mailto:hola@ws-dyr.com" // ← poné tu mail acá
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              src={bannerMail}
+              alt="Contacto por Mail"
+              width={1200}
+              height={316}
+              className={styles.bannerBottomImg}
+            />
+          </a>
+        </div>
       </div>
     </section>
   );
